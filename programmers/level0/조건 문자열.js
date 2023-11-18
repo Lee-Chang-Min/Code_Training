4f17a49aa98e872eaeaae2e4b5d3a1f1931f4585
ChangMin\ChangMin <cmlee>
2023-11-18 16:24:29 +0900

    2023-11-18 프로그래머스

---
+++ programmers/level0/조건 문자열.js
@@ -0,0 +1,17 @@
+function solution(ineq, eq, n, m) {
+
+    
+    if(ineq === ">" && eq === "="){
+        return n >= m ? 1 : 0
+    }else if (ineq === "<" && eq === "=") {
+        return n <= m ?  1 : 0
+    }else if (ineq === ">" && eq === "!"){
+        return n > m ?  1 : 0
+    }else if (ineq === "<" && eq === "!"){
+        return n < m ? 1 : 0
+    }
+    
+  
+}
+
+solution("<","=",1,2)
