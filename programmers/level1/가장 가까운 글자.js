6739eeddd93059f865f0865a34e845dcf373a2c5
LCM <10billionmin@naver.com>
2024-01-26 17:04:33 +0900

    20240126

---
+++ programmers/level1/가장 가까운 글자.js
@@ -0,0 +1,13 @@
+function solution(s) {
+    var answer = [];
+    return answer;
+}
+
+
+let s = 'banana'
+
+console.log(Array.from(s))
+
+Array.from(s).forEach((element, i)=> {
+    
+});
--- programmers/level1/개인정보 수집 유효기간.js
+++ programmers/level1/개인정보 수집 유효기간.js
@@ -5,25 +5,31 @@ function solution(today, terms, privacies) {
 var answer = [];
 
 
-let today = "2022.05.19"
-let terms = ["A 6", "B 12", "C 3"]
-let privacies = ["2021.05.02 A", "2021.07.01 B", "2022.02.19 C", "2022.02.20 C"]
+let today =  "2020.05.15"
+let terms = ["A 1"]
+let privacies = ["2001.01.10 A", "2001.01.10 A"]
 
 let today1 = new Date(today);
-for(let p of privacies){
+
+
+for(let [i,p] of privacies.entries()){
     let pdate = new Date(p.split(' ')[0]);
     let ptype = p.split(' ')[1];
     
     
     let checkTerms = terms.filter((v, i) => ptype === v.split('')[0]);
     //["A 6"]
-
+    
     let termsMonth = Number(checkTerms[0].split(' ')[1]);
 
-    if(pdate.getMonth() + termsMonth > 12 ){
+    pdate.setMonth(pdate.getMonth() + termsMonth );
 
-        let year = today.getFullYear() +(pdate.getMonth() + termsMonth % 12)
-        Number(pdate.split('.')[0]) + Number(pdate.split('.')[1]) + Number(checkTerms[0].split(' ')[1]) % 12
+    if(pdate <= today1) {
+        answer.push(i+1)
     }
 
 }
+
+console.log(answer);
+
+"2010.01.01", ["A 12"], ["2009.12.01 A", "2001.01.01 A"]
