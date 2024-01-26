2f4d2c1ae6d645ef5d59b38a5a461c4ede4324ea
LCM <10billionmin@naver.com>
2024-01-26 17:28:39 +0900

    가장 가까운 글자

--- programmers/level1/가장 가까운 글자.js
+++ programmers/level1/가장 가까운 글자.js
@@ -3,11 +3,22 @@ function solution(s) {
     return answer;
 }
 
-
+var answer = [];
 let s = 'banana'
 
 console.log(Array.from(s))
-
-Array.from(s).forEach((element, i)=> {
-    
+Array.from(s).forEach((e, idx)=> {
+    let n = 0;
+    while(s[idx-1] !== undefined){
+        n++
+        console.log( s[idx-1]);
+        if(e == s[idx-1]) {
+            answer.push(n); 
+            break;
+        }
+        idx--;
+    }
+    answer.push(-1)
 });
+
+console.log(answer);
