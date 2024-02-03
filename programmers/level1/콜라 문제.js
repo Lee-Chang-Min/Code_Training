ba9c0bea392341efaec58335d7c9af7be46930f0
ChangMin\ChangMin <10billionmin@naver.com>
2024-02-03 15:55:44 +0900

    '20240203콜라문제'

--- programmers/level1/콜라 문제.js
+++ programmers/level1/콜라 문제.js
@@ -13,8 +13,9 @@ while(n >= a){
     
     //교환해서 받은 병수
     console.log(((Math.floor(n/a) * a) / a) * b);
+    answer += ((Math.floor(n/a) * a) / a) * b;
     
     //교환하고 집에 남은 병수
     n = n - (Math.floor(n/a) * a) + (((Math.floor(n/a) * a) / a) * b);
-    answer += n;
 }
+
