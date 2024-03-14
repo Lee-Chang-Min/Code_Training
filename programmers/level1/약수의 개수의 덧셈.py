61068f8efaf6ffc0584aff9ca40839bf0a0bdaaf
LCM <10billionmin@naver.com>
2024-03-14 16:53:33 +0900

    약수의 갯수의 덧셈

---
+++ programmers/level1/약수의 개수의 덧셈.py
@@ -0,0 +1,29 @@
+
+
+def solution(left, right):
+    answer = 0
+
+    for key in range(left, right+1):
+        count = 0
+        
+        for i in range(1, key + 1):
+            if (key % i == 0) :
+
+                count += 1
+        print(count / 2) 
+        answer += key if count % 2 == 0 else -key
+
+    return answer
+
+left = 13
+rigth = 17
+
+answer = 0
+divisorsList = [];
+    
+    
+print(answer)
+
+        
+        
+
