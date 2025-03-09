cbf966aaf51caa50b35bde57d0b414ddff12e760
LeeChangMin <10billionmin@naver.com>
2025-03-09 20:23:47 +0900

    구슬탈줄 2

---
+++ programmers/inflearn/딩코딩코/5주차/구슬탈출2.py
@@ -0,0 +1,87 @@
+import sys
+
+
+def move(i,j,dr):
+    back = -1
+    for cnt in range(1, 10): # 벽을 만날때까지
+        ni,nj = i+di[dr]*cnt, j+dj[dr]*cnt
+        if arr[ni][nj] == '#': return cnt+back
+        if arr[ni][nj] == 'O': return cnt
+
+        # 다른 공을 지나간 경우 벽을 만났다면 한칸 뒤로
+        if arr[ni][nj] == 'B' or arr[ni][nj] == 'R': back -= 1
+
+
+
+def dfs(n, ri,rj,bi,bj):
+    global ans
+    
+    # if (n, ri,rj,bi,bj) in v_set: # 이미 이 시도횟수에 이 좌표를 해봤음 => 가지치기
+    #     return
+    # v_set.add(n,ri,rj,bi,bj)
+
+
+    if n > 10: 
+        return # 10회 이하까지만 진행
+
+    # 4방향 처리
+    for dr in range(4):
+        # [1] 각 공의 이동거리 계산
+        r_cnt = move(ri,rj,dr) # 해당 방향으로 이동거리
+        b_cnt = move(bi,bj,dr)
+        if r_cnt == 0 and b_cnt == 0 : # 공이 움직이지 않으니 탐색 종료
+            continue
+        
+        # [2] 각공의 이동 반영
+        nri, nrj = ri + di[dr]*r_cnt, rj + dj[dr]*r_cnt
+        nbi, nbj = bi + di[dr]*b_cnt, bj + dj[dr]*b_cnt
+
+        #구망에 대한 처리 (빨간색만 들어가면 성공, 파란색이 들어가면 실패)
+        #[3] 이동한 위치가 홀인 경우 처리
+        if arr[nbi][nbj] == 'O':
+            continue
+        else:
+            if arr[nri][nrj] == 'O' : # 빨간색 공만
+                ans=min(ans, n)
+                return
+            
+        #[4] 둘다 홀이 아닌 경우 (next 좌표 기준으로 다음 시도)
+        # 현재 위치를 빈칸, 이동할 위치에 'R', 'B' 구슬 표시
+        arr[ri][rj], arr[bi][bj] = '.', '.'
+        arr[nri][nrj], arr[nbi][nbj] = 'R', 'B'
+
+        dfs(n+1, nri,nrj,nbi,nbj)
+
+        #원상복구
+        arr[nri][nrj], arr[nbi][nbj] = '.', '.' 
+        arr[ri][rj], arr[bi][bj] = 'R', 'B'
+
+
+di = [-1, 1, 0, 0]
+dj = [0, 0, -1, 1]
+
+N, M = map(int, input().split())
+arr = [list(input()) for _ in range(N)]
+
+# print(arr)
+# [['#', '#', '#', '#', '#'], ['#', '.', '.', 'B', '#'], ['#', '.', '#', '.', '#'], ['#', 'R', 'O', '.', '#'], ['#', '#', '#', '#', '#']]
+
+
+# ri rj bj bj 구슬의 위치
+for i in range(N):
+    for j in range(M):
+        if arr[i][j] == 'R' :
+            ri, rj = i, j
+        if arr[i][j] == 'B' :
+            bi, bj = i, j
+
+
+# v_set = set()
+
+ans = 11
+dfs(1, ri, rj, bi, bj)
+
+if ans == 11 :
+    ans = -1
+
+print(ans)
--- programmers/inflearn/딩코딩코/5주차/새로운 게임2.py
+++ programmers/inflearn/딩코딩코/5주차/새로운 게임2.py
@@ -1,26 +1,26 @@
 
+
+
 k = 4  # 말의 개수
 
 # 0은 흰색, 1은 빨간색, 2는 파란색
 chess = [
-    [0, 0, 2, 0],
-    [0, 0, 1, 0],
-    [0, 0, 1, 2],
-    [0, 2, 0, 0]
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
 ]
 
-# 0,1,2,3
-#오,왼, 위,아래
-start_horse = [
-    [0, 0, 0],
-    [0, 1, 0],
-    [0, 2, 0],
-    [2, 2, 2]
-]
-
-
+# 동 서 북 남
+# →, ←, ↑, ↓
 dr = [0,0,-1,1]
 dc = [1,-1,0,0]
+
+def goback(d):
+    if d % 2 == 0:
+        return d + 1
+    else:
+        return d - 1
 
 
 # 아 이거 쌓인 순서를 지정 해야함 -> 맵에서 어떻게 체스 말들이 쌓여져 있는지 저장해야 됨
@@ -28,27 +28,86 @@ dc = [1,-1,0,0]
 # 턴 한번은 1번 말부터 K번 말까지 순서대로 이동 -> 반복문을 통해 각 말을 이동 시키는 것이 필요
 
 #말의 현재 위치
-current = [
-    [[0],[1],[2],0],
-    [0,0,0,0],
-    [0,0,[3],0],
-    [0,0,0,0]
-]
+# current = [
+#     [[0],[1],[2],0],
+#     [0,0,0,0],
+#     [0,0,[3],0],
+#     [0,0,0,0]
+# ]
 
 
 def getgameover (horse_count, game_map, horse_loc):
     n = len(game_map)
+    turn_count = 1
     print("n =",n)
 
     current = [[[] for _ in range(n)] for _ in range(n)]
-    print(current)
+    for i in range(horse_count):
+        r, c, d = horse_loc[i]
+        current[r][c].append(i)
+    # print(current)
+    
+    while turn_count <=1000:
+        for horse_index in range(horse_count): #horse_index 는 몇번째 말인지
+            r, c, d = horse_loc[horse_index]
 
-    return
+            new_r, new_c = r + dr[d], c+dc[d]
+
+            #파란색의 경우
+            if not 0 <= new_r < n or not 0 <= new_c < n or game_map[new_r][new_c] == 2:
+                new_d = goback(d)
+
+                # 이동방향을 반대로 하고 한칸 이동, 방향을 바꾼 후 이동하려는 칸이 파란색인 경우는 이동하지 않고 가만히 있는다.
+                new_r, new_c = r+dr[new_d], c+dc[new_d]
+                horse_loc[horse_index][2] = new_d
+
+                if not 0 <= new_r < n or not 0 <= new_c < n or game_map[new_r][new_c] == 2:
+                    continue
 
 
-print(getgameover(k,chess,start_horse))
+
+            # 흰색의 경우 칸 이동, 이동한 칸에 말이 있는 경우 가장 위에 나의 말을 올려놓음
+            # 말을 같이 이동함
+            # 추가로 1,2,3 이 쌓여있고 이동하려는 칸에 4,5가 있는경우 4,5,1,2,3
+
+            moving_horse = []
+            for i in range(len(current[r][c])):
+                current_index = current[r][c][i]  
+
+                if horse_index == current_index:
+                    moving_horse = current[r][c][i:]
+                    current[r][c] = current[r][c][:i] # 말이 이동했으니 원래 위치에서는 지워 줘야 함
+                    break
+                
+                # 빨간색인 경우에는 이동한 후 이동한 말에 대해 모든 말의 쌓여있는 순서를 바꾼다 
+            if game_map[new_r][new_c] == 1:
+                moving_horse = reversed(moving_horse) 
+
+            
+            #이제 새로운 곳에 말을 넣어주면 됌
+            for moving_horse_i in moving_horse:
+                current[new_r][new_c].append(moving_horse_i)
+                horse_loc[moving_horse_i][0], horse_loc[moving_horse_i][1] = new_r, new_c #말의 위치가 이동했으니 업데이트 
+
+            if len(current[new_r][new_c]) >=4:
+                return turn_count
+        
+        turn_count +=1
 
 
+    return -1
+
+
+# print(getgameover(k,chess,start_horse))
+
+
+start_horse = [
+    [0, 1, 0],
+    [1, 1, 0],
+    [0, 2, 0],
+    [2, 2, 2]
+]
+print("정답 = 9 / 현재 풀이 값 = ", getgameover(k, chess, start_horse))
 
 
 ###############################
