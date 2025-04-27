6bedc63f4db06b289dfdb76917e5eff29a3e0e72
ChangMin <10billionmin@naver.com>
2025-04-27 21:49:12 +0900

    호텔 대실

--- programmers/백준/쿠키 3번.py
+++ programmers/백준/쿠키 3번.py
@@ -1,42 +1,4 @@
-# def sol(pouches):
-#     flavors = ['a', 'b', 'c', 'd', 'e']
-#     max_selected = 0  # 모든 맛 중 선택 가능한 최대 주머니 수
 
-#     # 각 맛에 대해 반복
-#     for f in flavors:
-#         contributions = []  # 각 주머니가 해당 맛을 얼마나 돋보이게 하는지 저장할 리스트
-
-#         # 각 주머니에 대해 해당 맛의 젤리 개수와 다른 맛의 젤리 개수를 계산
-#         for pouch in pouches:
-#             count_chosen = pouch.count(f)       # 주머니에서 맛 f의 젤리 개수
-#             count_total = len(pouch)              # 주머니의 전체 젤리 개수
-#             count_others = count_total - count_chosen  # 주머니에서 다른 맛의 젤리 개수
-#             # '초과분'은 맛 f의 젤리 개수에서 다른 맛 젤리 개수를 뺀 값입니다.
-#             # 이 값이 클수록 그 주머니는 맛 f를 돋보이게 합니다.
-#             excess = count_chosen - count_others
-#             contributions.append(excess)
-
-#         # 가장 많이 기여하는 주머니부터 선택하기 위해 내림차순 정렬
-#         contributions.sort(reverse=True)
-
-#         total_excess = 0  # 선택한 주머니들의 '초과분' 누적합
-#         selected = 0      # 선택한 주머니의 수
-
-#         # 내림차순으로 정렬된 주머니들을 하나씩 선택하면서 누적 '초과분'을 구합니다.
-#         for value in contributions:
-#             total_excess += value
-#             # 누적 '초과분'이 양수이면, 전체적으로 맛 f의 젤리 개수가 다른 젤리 개수보다 많은 상태입니다.
-#             if total_excess > 0:
-#                 selected += 1
-#             else:
-#                 # 누적 '초과분'이 0 이하가 되면 더 이상 주머니를 추가하면 조건이 깨지므로 종료합니다.
-#                 break
-
-#         # 현재 맛 f로 선택한 주머니 수와 지금까지의 최대값을 비교하여 업데이트합니다.
-#         if selected > max_selected:
-#             max_selected = selected
-
-#     return max_selected
 
 def solution(pouches):
 
