e173b6cab2724aaf48223db949d2ca5dbda4f149
이창민 <lcm@ichangmin-ui-MacBookPro.local>
2025-06-08 21:47:42 +0900

    숫자 변환하기

---
+++ programmers/level2/숫자변환하기.py
@@ -0,0 +1,22 @@
+from collections import deque
+
+def solution(x, y, n):
+    q = deque()
+    q.append((x, 0))
+    visited = set()
+
+    while q:
+        num, cnt = q.popleft()
+
+        if num > y or num in visited:
+            continue
+        visited.add(num)
+        
+        if num == y:
+            return cnt
+
+        for k in (num*2, num*3, num+n):
+            if k <= y and k not in visited:
+                q.append((k, cnt + 1))
+
+    return -1
